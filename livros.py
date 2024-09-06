308289d9c84303fb6e257f96a3fd625688f24b42
Francisco de Assis <fappinheirodev@gmail.com>
2024-09-06 16:50:59 -0300

    listar livros

--- livros.py
+++ livros.py
@@ -1,8 +1,12 @@
 livros = []
 
-def add_book():
+def add_book(livros):
     add_book = input("adicione um livro: ")
     livros.append(add_book)
+
+def listar_livros():
+    for livro in livros:
+        
 
 add_book()
 print(livros)
